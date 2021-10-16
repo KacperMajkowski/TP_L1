bd4e5814ae3a586c6e482a25d65bc1769ae0761f
KacperMajkowski <kacper20015@o2.pl>
2021-10-16 21:41:14 +0200

    commit2

--- src/main/java/MainGame.java
+++ src/main/java/MainGame.java
@@ -5,7 +5,7 @@ public class MainGame {
     {
         Scanner scan = new Scanner(System.in);
 
-        GameBoard Board = new GameBoard(20);
+        GameBoard Board = new GameBoard(6);
 
         Player Player = new Player(Board.getSize()/2, Board.getSize()/2);
 
