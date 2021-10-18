5befd8e4ee4a3a2a67456d6232de44dd74badcc2
KacperMajkowski <kacper20015@o2.pl>
2021-10-18 22:25:06 +0200

    Git3

--- src/main/java/org/example/TPL1/MainGame.java
+++ src/main/java/org/example/TPL1/MainGame.java
@@ -8,7 +8,7 @@ public class MainGame {
     {
         Scanner scan = new Scanner(System.in);
 
-        GameBoard Board = new GameBoard(12);
+        GameBoard Board = new GameBoard(16);
 
         Player Player = new Player(Board.getSize()/2, Board.getSize()/2);
 
